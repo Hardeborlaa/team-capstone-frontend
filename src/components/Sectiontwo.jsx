06461d5408d6590e3d02d813aad659664d64b074
somtochukwu nwosu <somynwosu@gmail.com>
2022-12-14 09:47:06 +0100

    first commit

--- src/components/Sectiontwo.jsx
+++ src/components/Sectiontwo.jsx
@@ -9,14 +9,14 @@ const Sectiontwo = () => {
   return (
     <div className='min-h-screen w-[100%] mx-auto px-24 grid grid-cols-2 gap-6'>
       <div>
-        <img src={two} className="col-span-2" />
+        <img src={two} className="col-span-2" alt=""/>
             <div className='grid grid-cols-2 py-4'>
                 <img src={three} alt="" />
                 <img src={four} alt="" />
             </div>
       </div>
       <div>
-        <img src={ten} className="row-span-1 w-[71%] h-auto img-ten" />
+        <img src={ten} className="row-span-1 w-[71%] h-auto img-ten" alt="" />
       </div>    
       </div> 
              
