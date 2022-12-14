9daed15e2107a897d0cc422debf3da42a269cf27
Adeleye Opeyemi <adebola786@gmail.com>
2022-12-14 11:45:07 +0100

    heading update

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -7,7 +7,7 @@ const Shop = () => {
         <div className="max-w-[1440px] mx-auto">
             <div className="relative">
                 <div className="absolute w-full h-full text-black-200 max-h-[500px] sm:inset-12 lg:inset-44 my-16">
-                    <h4>OWN THE DAY</h4>
+                    <h3>OWN THE DAY</h3>
                     <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold my-3">TACHEN 19</h1>
                     <button className="bg-black text-white w-[200px] rounded-md font-medium mx-auto md:mx-0 py-3">SHOP COLLECTION</button>
                 </div>
