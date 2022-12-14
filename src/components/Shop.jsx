6098c956fc95c712aa474c51cd605d83c11bcde8
Adeleye Opeyemi <adebola786@gmail.com>
2022-12-14 10:22:51 +0100

    update footer section

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -5,7 +5,7 @@ import Women from '../assets/Women.jpeg';
 const Shop = () => {
     return (  
         <div className="max-w-[1440px] mx-auto">
-            <div className="max-h-[500px] relative">
+            <div className="relative">
                 <div className="absolute w-full h-full text-black-200 max-h-[500px] sm:inset-12 lg:inset-44 my-16">
                     <h4>OWN THE DAY</h4>
                     <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold my-3">TACHEN 19</h1>
